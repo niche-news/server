9dc9ca6b95e32a3935fbb7621e2a7f1ec996ef18
Alden <aldenblamp@gmail.com>
2018-07-11 13:44:05 -0400

    Creating server file

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def indexPage():
+	return "welcome to nicheNews"
+
+@app.route('/getArticles', methods=['POST'])
+def getArticles():
+	
+
+if __name__ == '__main__':
+	app.run(debug=True, port=55622)
